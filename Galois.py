3ce0311c33ab32438b96a259035d7f0047b782df
dominusmi <dominusmi@users.noreply.github.com>
2017-06-07 00:39:21 +0100

    Created main thread and added hook to GPS signal

---
+++ Galois.py
@@ -0,0 +1,53 @@
+import serial
+import sys
+import os
+from Serial_hook import Serial_hook
+from threading import Thread
+from time import sleep
+
+## E/F Le Galois ##
+
+## Boolean variable that will represent
+## whether or not GPS connected
+
+connected = False
+
+
+## Main loop
+while True:
+
+    ## Catches KeyboardInterrupt
+    try:
+
+        if( connected == False ):
+            device='/dev/ttyACM0'
+            ser = None
+            try:
+                ## Sets ttyACM0 to baudrate 9600, and then tries to connect
+                print 'Trying to connect to', device
+                os.system('stty -F /dev/ttyACM0 9600')
+                ser = serial.Serial(device, 9600)
+
+                ## If no exception --> all goes well, set connected accordingly
+                connected=True
+            except:
+                print "Failed to connect on",device
+                sys.exit()
+
+            ## Creates thread which handles writing GPS data to file
+            if( connected ):
+                try:
+                    serial_hook = Serial_hook(ser)
+                except Exception as e:
+                    print "Error on thread"
+                    sys.exit()
+
+        # No need to be constantly running
+        sleep(0.1)
+
+    except KeyboardInterrupt:
+        print "Ah! KeyboardInterrupt!"
+        ## Stops the thread
+        serial_hook.stop()
+        ## Clean exit
+        sys.exit(0)
